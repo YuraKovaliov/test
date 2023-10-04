5bd3465f8356791c753a42d403c47d80c3c78c8d
YuraKovaliov <146247118+YuraKovaliov@users.noreply.github.com>
2023-10-04 22:41:47 +0300

    112

--- index.php
+++ index.php
@@ -37,9 +37,116 @@ $secondArr = [
 // $Difference = array_diff ($firstArr, $secondArr );
 // print_r($Difference);
  $Difference = array_merge (array_diff ($secondArr, $firstArr), array_diff ($firstArr, $secondArr));
+ echo "<pre>";
  print_r($Difference);
- 123
+ echo "<pre>";
 
+print_r(array_uintersect($firstArr, $secondArr,"strcasecmp"));
+
+$result = array_diff_assoc($firstArr, $secondArr);
+print_r($result);
+//
+//$FirstArr = [
+//
+//    'one' => 1,
+//
+//    'two' => [
+//
+//        'one' => 1,
+//
+//        'seven' => 22,
+//
+//        'three' => 32,
+//
+//    ],
+//
+//    'three' => [
+//
+//        'one' => 1,
+//
+//        'two' => 2,
+//
+//    ],
+//
+//    'foure' => 5,
+//
+//    'five' => [
+//
+//        'three' => 32,
+//
+//        'foure' => 5,
+//
+//        'five' => 12,
+//
+//    ],
+//
+//];
+//print_r( array_slice( $directors, 1, 2 ) );
+//foreach ($arrays as $array) {
+// echo '<pre>';
+// echo $array[1,];
+//}
+
+
+//echo count($FirstArr);
+$FirstArr = [
+    'one' => 1,
+    'two' => [
+        'one' => 1,
+        'seven' => 22,
+        'three' => 32,
+    ],
+    'three' => [
+        'one' => 1,
+        'two' => 2,
+    ],
+    'foure' => 5,
+    'five' => [
+        'three' => 32,
+        'foure' => 5,
+        'five' => 12,
+    ],
+];
+
+$secondElements = [];
+
+$index = 0;
+foreach ($FirstArr as $key => $value) {
+ if ($index % 2 !== 0) {
+  $secondElements[$key] = $value;
+ }
+ $index++;
+}
+
+print_r($secondElements);
+
+function countRecursive($array) {
+ $count = count($array);
+ foreach ($array as $value) {
+  if (is_array($value)) {
+   $count += countRecursive($value);
+  }
+ }
+ return $count;
+}
+
+$totalElements = countRecursive($FirstArr);
+echo $totalElements ;
+echo "<br>";
+function sumRecursive($array) {
+ $sum = 0;
+ foreach ($array as $value) {
+  if (is_array($value)) {
+   $sum += sumRecursive($value);
+  } else {
+   $sum += $value;
+  }
+ }
+ return $sum;
+}
+
+$totalSum = sumRecursive($FirstArr);
+echo $totalSum ;
 
 
 ?>
