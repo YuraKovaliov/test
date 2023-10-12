14b63aa1eafe43058641f8da48315145495668e7
YuraKovaliov <146247118+YuraKovaliov@users.noreply.github.com>
2023-10-12 14:28:11 +0300

    123

--- index.php
+++ index.php
@@ -1,152 +1,256 @@
 <?php
-$arr = [1, 2, 3, 7, 31, 4, 1, 8, 6];
-//var_dump($arr) ;
-echo count($arr);
-echo "<br>";
-echo $arr[4] + $arr[5] + $arr[6];
+function doesElementExistInNestedArray($array, $element) {   //Задание 1
 
-$firstArr = [
-
-  'one' => 1,
-
-  'two' => 2,
-
-  'three' => 3,
-
-  'foure' => 5,
-
-  'five' => 12,
-
-];
-
-$secondArr = [
-
-  'one' => 1,
-
-  'seven' => 22,
-
-  'three' => 32,
-
-  'foure' => 5,
-
-  'five' => 13,
-
-  'six' => 37,
-
-];
-// $Difference = array_diff ($firstArr, $secondArr );
-// print_r($Difference);
- $Difference = array_merge (array_diff ($secondArr, $firstArr), array_diff ($firstArr, $secondArr));
- echo "<pre>";
- print_r($Difference);
- echo "<pre>";
-
-print_r(array_uintersect($firstArr, $secondArr,"strcasecmp"));
-
-$result = array_diff_assoc($firstArr, $secondArr);
-print_r($result);
-//
-//$FirstArr = [
-//
-//    'one' => 1,
-//
-//    'two' => [
-//
-//        'one' => 1,
-//
-//        'seven' => 22,
-//
-//        'three' => 32,
-//
-//    ],
-//
-//    'three' => [
-//
-//        'one' => 1,
-//
-//        'two' => 2,
-//
-//    ],
-//
-//    'foure' => 5,
-//
-//    'five' => [
-//
-//        'three' => 32,
-//
-//        'foure' => 5,
-//
-//        'five' => 12,
-//
-//    ],
-//
-//];
-//print_r( array_slice( $directors, 1, 2 ) );
-//foreach ($arrays as $array) {
-// echo '<pre>';
-// echo $array[1,];
-//}
-
-
-//echo count($FirstArr);
-$FirstArr = [
-    'one' => 1,
-    'two' => [
-        'one' => 1,
-        'seven' => 22,
-        'three' => 32,
-    ],
-    'three' => [
-        'one' => 1,
-        'two' => 2,
-    ],
-    'foure' => 5,
-    'five' => [
-        'three' => 32,
-        'foure' => 5,
-        'five' => 12,
-    ],
-];
-
-$secondElements = [];
-
-$index = 0;
-foreach ($FirstArr as $key => $value) {
- if ($index % 2 !== 0) {
-  $secondElements[$key] = $value;
- }
- $index++;
-}
-
-print_r($secondElements);
-
-function countRecursive($array) {
- $count = count($array);
  foreach ($array as $value) {
-  if (is_array($value)) {
-   $count += countRecursive($value);
-  }
- }
- return $count;
-}
 
-$totalElements = countRecursive($FirstArr);
-echo $totalElements ;
-echo "<br>";
-function sumRecursive($array) {
- $sum = 0;
- foreach ($array as $value) {
   if (is_array($value)) {
-   $sum += sumRecursive($value);
+   if (doesElementExistInNestedArray($value, $element)) {
+    return true;
+   }
   } else {
-   $sum += $value;
+
+   if ($value === $element) {
+    return true;
+   }
   }
  }
+
+ return false;
+
+
+
+function countBsInString($input) {  //Задание 2
+ if (!is_string($input)) {
+  return false;
+ }
+
+ return substr_count($input, 'b');
+}
+
+function sumArrayRecursive($array) {
+ $sum = 0;
+
+
+ foreach ($array as $element) { //Задание 3
+  if (is_array($element)) {
+   $sum += sumArrayRecursive($element); // Рекурсивный
+  } else {
+   $sum += $element;
+  }
+ }
+
  return $sum;
 }
 
-$totalSum = sumRecursive($FirstArr);
-echo $totalSum ;
+function countSquaresFit($bigSquareSide, $smallSquareSide) { //Задание 4
+ // Вычисляем
+ $bigSquareArea = $bigSquareSide * $bigSquareSide;
+
+ // Вычисляем площадь
+ $smallSquareArea = $smallSquareSide * $smallSquareSide;
+
+ // Вычисляем количество
+ $count = $bigSquareArea / $smallSquareArea;
+
+ return $count;
+}
+
+
+abstract class MathOperation {
+ protected $base;
+ protected $exponent;
+
+ public function getBase() {
+  return $this->base;
+ }
+
+ public function setBase($base) {
+  $this->base = $base;
+ }
+
+ public function getExponent() {
+  return $this->exponent;
+ }
+
+ public function setExponent($exponent) {
+  $this->exponent = $exponent;
+ }
+
+ abstract public function calculatePower();
+}
+
+
+
+
+
+abstract class MathOperation {
+ protected $base;
+ protected $exponent;
+
+ public function getBase() {
+  return $this->base;
+ }
+
+ public function setBase($base) {
+  $this->base = $base;
+ }
+
+ public function getExponent() {
+  return $this->exponent;
+ }
+
+ public function setExponent($exponent) {
+  $this->exponent = $exponent;
+ }
+
+ abstract public function calculatePower();
+}
+
+class AdditionOperation extends MathOperation {
+ private $operand;
+
+ public function getOperand() {
+  return $this->operand;
+ }
+
+ public function setOperand($operand) {
+  $this->operand = $operand;
+ }
+
+ public function calculatePower() {
+  return $this->getBase() + $this->operand;
+ }
+}
+
+class SubtractionOperation extends MathOperation {
+ private $operand;
+
+ public function getOperand() {
+  return $this->operand;
+ }
+
+ public function setOperand($operand) {
+  $this->operand = $operand;
+ }
+
+ public function calculatePower() {
+  return $this->getBase() - $this->operand;
+ }
+}
+
+class MultiplicationOperation extends MathOperation {
+ private $factor;
+
+ public function getFactor() {
+  return $this->factor;
+ }
+
+ public function setFactor($factor) {
+  $this->factor = $factor;
+ }
+
+ public function calculatePower() {
+  return $this->getBase() * $this->factor;
+ }
+}
+
+
+
+
+
+abstract class MathOperation {
+    protected $base;
+    protected $exponent;
+
+    public function getBase() {
+        return $this->base;
+    }
+
+    public function setBase($base) {
+        $this->base = $base;
+    }
+
+    public function getExponent() {
+        return $this->exponent;
+    }
+
+    public function setExponent($exponent) {
+        $this->exponent = $exponent;
+    }
+
+    abstract public function calculatePower();
+}
+
+class AdditionOperation extends MathOperation {
+    private $operand;
+
+    public function getOperand() {
+        return $this->operand;
+    }
+
+    public function setOperand($operand) {
+        $this->operand = $operand;
+    }
+
+    public function calculatePower() {
+        return $this->getBase() + $this->operand;
+    }
+
+    public function doCustomOperation() {
+        return $this->getBase() * $this->operand;
+    }
+}
+
+class SubtractionOperation extends MathOperation {
+    private $operand;
+
+    public function getOperand() {
+        return $this->operand;
+    }
+
+    public function setOperand($operand) {
+        $this->operand = $operand;
+    }
+
+    public function calculatePower() {
+        return $this->getBase() - $this->operand;
+    }
+
+    public function doCustomOperation() {
+        return $this->getBase() / $this->operand;
+    }
+}
+
+class MultiplicationOperation extends MathOperation {
+    private $factor;
+
+    public function getFactor() {
+        return $this->factor;
+    }
+
+    public function setFactor($factor) {
+        $this->factor = $factor;
+    }
+
+    public function calculatePower() {
+        return $this->getBase() * $this->factor;
+    }
+
+    public function doCustomOperation() {
+        return $this->getBase() ** $this->factor;
+    }
+}
+
+class SquareOperation extends AdditionOperation {
+    public function
+
+
+
+
+
+
+
+
 
 
 ?>
